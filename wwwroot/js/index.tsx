0d2105c3a8d44c295b999f9e04f83d5b3bf3b1d6
Alan Hape <ahape@brightmetrics.com>
2024-07-25 11:47:50 -0700

    JS/TS stuff

---
+++ wwwroot/js/index.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client';
+
+const id: string = "app";
+// Clear the existing HTML content
+document.body.innerHTML = `<div id="${id}"></div>`;
+
+// Render your React component instead
+
+const root = createRoot(document.getElementById(id)!);
+root.render(<h1>Hello, world</h1>);
